d94ccd3b9c66aa36fcbdfdbffce46b948f4678bb
mehmetsimsir.1994 <mehmetsimsir.1994@gmail.com>
2021-04-06 16:35:33 +0300

    initialize project

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import bodyparser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+const app = express();
+
+dotenv.config();
+app.use(bodyparser.json({ extended: true }));
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+  )
+  .catch((error) =>
+    console.log("Error has occured while connection: " + error.message)
+  );
+
+mongoose.set("useFindAndModify", false);
